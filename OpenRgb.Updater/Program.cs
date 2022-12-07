385c8766f7d68c9bf53795b2b8cd2478f7eea597
IceBeach7 <94013083+IceBeach7@users.noreply.github.com>
2022-12-07 20:33:56 +0100

    Added Linux support (#2)

--- OpenRgb.Updater/Program.cs
+++ OpenRgb.Updater/Program.cs
@@ -10,56 +10,122 @@ internal static class Program
 	{
 		Console.Title = "OpenRGB Updater";
 
+		if(!OperatingSystem.IsWindows() && !OperatingSystem.IsLinux())
+		{
+			throw new PlatformNotSupportedException();
+		}
 
-		var filePathZip = Path.Combine(Path.GetTempPath(), "OpenRGBTemp.zip");
-		var dirExtracted = Path.Combine(Path.GetTempPath(), "OpenRGB");
-		var dirInstallOpenRgb = Path.Combine(Environment.GetEnvironmentVariable("LOCALAPPDATA"), "OpenRGB");
-		var dirInstallPlugin = Path.Combine(Environment.GetEnvironmentVariable("APPDATA"), @"OpenRGB\plugins");
+
+		DirectoryInfo tempDir;
+		DirectoryInfo localAppDataDir;
+		DirectoryInfo appDataDir;
+
+		if(OperatingSystem.IsWindows())
+		{
+			tempDir = new(Environment.GetEnvironmentVariable("TEMP"));
+			localAppDataDir = new(Environment.GetEnvironmentVariable("LOCALAPPDATA"));
+			appDataDir = new(Environment.GetEnvironmentVariable("APPDATA"));
+		}
+		else
+		{
+			tempDir = new(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
+			localAppDataDir = new(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".applications"));
+			appDataDir = new(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+		}
+
+
+		FileInfo artifactFile = new(Path.Combine(tempDir.FullName, "OpenRGB-Updater.zip"));
+		DirectoryInfo extractDir = new(Path.Combine(tempDir.FullName, "OpenRGB"));
+		DirectoryInfo installDir = new(Path.Combine(localAppDataDir.FullName, "OpenRGB"));
+		DirectoryInfo pluginDir = new(Path.Combine(appDataDir.FullName, "OpenRGB/plugins"));
 
 		WebClient webClient = new();
 
 
-		if(File.Exists(filePathZip))
-		{
-			File.Delete(filePathZip);
-		}
+		var openRgbUrl = "https://gitlab.com/CalcProgrammer1/OpenRGB/-/jobs/artifacts/master/download?job=";
+		var pluginUrl = "https://gitlab.com/OpenRGBDevelopers/OpenRGBEffectsPlugin/-/jobs/artifacts/master/download?job=";
 
-		if(Directory.Exists(dirExtracted))
+		if(OperatingSystem.IsWindows())
 		{
-			Directory.Delete(dirExtracted, true);
+			openRgbUrl += "Windows%2064";
+			pluginUrl += "Windows%2064";
+		}
+		else if(OperatingSystem.IsLinux())
+		{
+			openRgbUrl += "Linux+64+AppImage";
+			pluginUrl += "Linux%2064";
 		}
 
 
 		Console.WriteLine("Downloading newest artifact of OpenRGB...");
-		webClient.DownloadFile("https://gitlab.com/CalcProgrammer1/OpenRGB/-/jobs/artifacts/master/download?job=Windows%2064", filePathZip);
+
+		if(artifactFile.Exists)
+		{
+			artifactFile.Delete();
+		}
+
+		webClient.DownloadFile(openRgbUrl, artifactFile.FullName);
+
 
 		Console.WriteLine("Extracting...");
-		ZipFile.ExtractToDirectory(filePathZip, dirExtracted);
-		File.Delete(filePathZip);
+
+		if(extractDir.Exists)
+		{
+			extractDir.Delete(true);
+		}
+
+		extractDir.Create();
+		ZipFile.ExtractToDirectory(artifactFile.FullName, extractDir.FullName);
+		artifactFile.Delete();
+
 
 		Console.WriteLine("Ending all OpenRGB processes...");
 		KillProcesses();
 		Thread.Sleep(2000);
 
+
 		Console.WriteLine("Replacing directory...");
-		if(Directory.Exists(dirInstallOpenRgb))
+
+		if(installDir.Exists)
 		{
-			Directory.Delete(dirInstallOpenRgb, true);
+			installDir.Delete(true);
+			installDir.Create();
 		}
 
-		Directory.Move(Path.Combine(dirExtracted, "OpenRGB Windows 64-bit"), dirInstallOpenRgb);
-		Directory.Delete(dirExtracted, true);
+		if(OperatingSystem.IsWindows())
+		{
+			Directory.Move(Path.Combine(extractDir.FullName, "OpenRGB Windows 64-bit"), installDir.FullName);
+		}
+		else
+		{
+			const string fileName = "OpenRGB-x86_64.AppImage";
+			File.Move(Path.Combine(extractDir.FullName, fileName), Path.Combine(installDir.FullName, fileName));
+		}
+
+		extractDir.Delete(true);
 
 
 		Console.WriteLine("Downloading newest artifact of the effects plugin...");
-		webClient.DownloadFile("https://gitlab.com/OpenRGBDevelopers/OpenRGBEffectsPlugin/-/jobs/artifacts/master/download?job=Windows%2064", filePathZip);
+		webClient.DownloadFile(pluginUrl, artifactFile.FullName);
+
 
 		Console.WriteLine("Extracting...");
-		ZipFile.ExtractToDirectory(filePathZip, dirInstallPlugin, true);
+		ZipFile.ExtractToDirectory(artifactFile.FullName, pluginDir.FullName, true);
 
 
 		Console.WriteLine("Executing OpenRGB...");
-		Process.Start("cmd.exe", $@"/C START """" ""{Path.Combine(dirInstallOpenRgb, "OpenRGB.exe")}""");
+
+		if(OperatingSystem.IsWindows())
+		{
+			Process.Start("cmd.exe", $@"/C START """" ""{Path.Combine(installDir.FullName, "OpenRGB.exe")}""");
+		}
+		else
+		{
+			FileInfo openRgbFile = new(Path.Combine(installDir.FullName, "OpenRGB-x86_64.AppImage"));
+
+			Process.Start("chmod", $@"+x ""{openRgbFile.FullName}""");
+			Process.Start(openRgbFile.FullName);
+		}
 
 
 		Console.Write("Done.");
